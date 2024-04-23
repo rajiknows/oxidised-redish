34d6414dcb04e994baad82dc8e18e91c1ed7e747
rajiknows <rajeshhjhamain@gmail.com>
2024-04-23 19:22:21 +0530

    pong

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    match listener.accept() {
-        Ok((_socket, addr)) => println!("new client: {addr:?}"),
-        Err(e) => println!("couldn't get client: {e:?}"),
-    }
+    //match listener.accept() {
+    //  Ok((_socket, addr)) => println!("new client: {addr:?}"),
+    //  Err(e) => println!("couldn't get client: {e:?}"),
+    //}
 
     for stream in listener.incoming() {
         match stream {
